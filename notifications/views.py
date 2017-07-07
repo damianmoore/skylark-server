42777035a090e777ebef34e6d79762625deed274
Damian Moore <damian@epixstudios.co.uk>
2017-07-07 01:27:00 +0100

    Registration API working via POSTing JSON. Basic webhook sending via FCM

--- notifications/views.py
+++ notifications/views.py
@@ -1,16 +1,43 @@
+import json
+
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from pyfcm import FCMNotification
 
 from .models import Device
 
 
+push_service = FCMNotification(api_key='AAAAyBt3BfQ:APA91bH3Sikwk947G8DAxqfin-vYgLZOJT_4mnOOQ56-M3JqRadXXo_qpRdeFzchirBqZmPiB2nqNZLIc_rcvcSOJ4jPPkplfAmtI-mAYtuvfjkBqdwUpS1wQzjOgpc4qVrnbY2QjwxD')
+
+
+@csrf_exempt
 def api_register(request):
-    if request.GET.get('id'):
-        device = Device.objects.get(id=request.GET['id'])
-        if request.GET.get('firebase_token'):
-            device.firebase_token = request.GET['firebase_token']
+    request_data = json.loads(request.read().decode('utf-8'))
+    if 'id' in request_data:
+        device = Device.objects.get(id=request_data['id'])
+        if 'firebase_token' in request_data:
+            device.firebase_token = request_data['firebase_token']
+        if 'name' in request_data:
+            device.name = request_data['name']
         device.save()
     else:
         device = Device()
-        device.name = request.GET.get('name')
+        device.name = request_data['name']
         device.save()
     return JsonResponse({'id': device.id})
+
+
+@csrf_exempt
+def webhook(request):
+    request_data = json.loads(request.read().decode('utf-8'))
+
+    registration_ids = [device.firebase_token for device in Device.objects.all()]
+    message_title = "Message title"
+    message_body = "Message body"
+    result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+
+    status = 'ERROR'
+    if result[0]['success'] == 1:
+        status = 'OK'
+
+    return JsonResponse({'status': status})
--- skylark/urls.py
+++ skylark/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from notifications.views import api_register
+from notifications.views import api_register, webhook
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/register/$', api_register),
+    url(r'^webhook/$', webhook),
 ]
