ae6c31694e284e51b6211295ed329b79a29c224f
Damian Moore <damian@epixstudios.co.uk>
2017-08-01 08:45:34 +0100

    Enhancements to list view filtering and searching

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Device, Notification, Filter
 
@@ -11,8 +12,14 @@ class DeviceAdmin(admin.ModelAdmin):
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ('title', 'webhook', 'created')
+    list_filter = ('webhook', 'created')
+    search_fields = ('title', 'webhook')
 
 
 @admin.register(Filter)
 class FilterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'title')
+    list_display = ('name', 'icon_preview', 'title')
+
+    def icon_preview(self, obj):
+        return format_html('<img src="{}" style="height: 50px" />', obj.icon)
+    icon_preview.short_description = 'Icon'
